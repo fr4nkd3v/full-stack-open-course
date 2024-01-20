6b185692d135d60579e8cb5d0a78bfe7637b084b
Fr4nkD3v <frank.code31@gmail.com>
2024-01-20 12:47:41 -0500

    chore: exercise 3.9 - phonebook backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'http://localhost:3000'
+const url = 'http://localhost:3001/api'
 
 const getAll = () => {
   return axios.get(`${url}/persons`).then(response => response.data)
